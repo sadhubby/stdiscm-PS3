86cc986148b22280dab369ec5868098f24ed6423
jamchuy <104501078+Jamchuyz@users.noreply.github.com>
2025-11-20 17:58:05 +0800

    more cleaning
    nag build failed for some annoying ass reason

--- ConsumerGUI/Form1.cs
+++ ConsumerGUI/Form1.cs
@@ -1,104 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-public partial class Form1 : Form
+namespace ConsumerGUI
 {
-    private List<VideoItem> videos = new List<VideoItem>();
-    private Timer previewTimer;
-    private PictureBox currentPreviewBox;
-
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
-        LoadMockVideos();
-        RenderThumbnails();
+        private List<VideoItem> videos = new List<VideoItem>();
+        private Timer previewTimer;
+        private PictureBox currentPreviewBox;
 
-        previewTimer = new Timer();
-        previewTimer.Interval = 300;
-        previewTimer.Tick += PreviewTimerTick;
-    }
-
-    private void LoadMockVideos()
-    {
-        videos.Add(new VideoItem(
-            "RivalsAhh.mp4", @"C:\Users\Nitro 5\Videos\MarvelRivals\Highlights\RivalsAhh.mp4" 
-        ));
-    }
-
-    private void RenderThumbnails()
-    {
-        flowPanelVideos.Controls.Clear();
-
-        foreach (var vid in videos)
+        public Form1()
         {
-            var pb = new PictureBox();
-            pb.Width = 160;
-            pb.Height = 100;
-            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+            InitializeComponent();
+            LoadMockVideos();
+            RenderThumbnails();
+            SetupPreviewTimer();
+        }
+
+        private void SetupPreviewTimer()
+        {
+            previewTimer = new Timer();
+            previewTimer.Interval = 500; // fake preview animation
+            previewTimer.Tick += PreviewTimerTick;
+        }
+
+        private void LoadMockVideos()
+        {
+            videos.Add(new VideoItem("RivalsAhh.mp4",
+                "C:\\Users\\Nitro 5\\Videos\\MarvelRivals\\Highlights\\RivalsAhh.mp4"));
+        }
+
+        private void RenderThumbnails()
+        {
+            flowPanelVideos.Controls.Clear();
+
+            foreach (var vid in videos)
+            {
+                PictureBox pb = new PictureBox
+                {
+                    Width = 200,
+                    Height = 120,
+                    SizeMode = PictureBoxSizeMode.StretchImage,
+                    BorderStyle = BorderStyle.FixedSingle,
+                    Image = Properties.Resources.default_thumb,
+                    Tag = vid
+                };
+
+                pb.MouseHover += ThumbnailMouseHover;
+                pb.MouseLeave += ThumbnailMouseLeave;
+                pb.Click += ThumbnailClick;
+
+                flowPanelVideos.Controls.Add(pb);
+            }
+        }
+
+        private void ThumbnailMouseHover(object sender, EventArgs e)
+        {
+            currentPreviewBox = sender as PictureBox;
+            currentPreviewBox.BorderStyle = BorderStyle.Fixed3D;
+            previewTimer.Start();
+        }
+
+        private void ThumbnailMouseLeave(object sender, EventArgs e)
+        {
+            previewTimer.Stop();
+            var pb = sender as PictureBox;
             pb.BorderStyle = BorderStyle.FixedSingle;
-
-            // Placeholder image
             pb.Image = Properties.Resources.default_thumb;
+        }
 
-            pb.Tag = vid;
+        private int previewFrame = 0;
 
-            pb.MouseHover += ThumbnailMouseHover;
-            pb.MouseLeave += ThumbnailMouseLeave;
-            pb.Click += ThumbnailClick;
+        private void PreviewTimerTick(object sender, EventArgs e)
+        {
+            if (currentPreviewBox == null) return;
 
-            flowPanelVideos.Controls.Add(pb);
+            previewFrame++;
+
+            // Fake animation
+            currentPreviewBox.BackColor =
+                (previewFrame % 2 == 0) ? Color.LightGray : Color.White;
+        }
+
+        private void ThumbnailClick(object sender, EventArgs e)
+        {
+            var pb = sender as PictureBox;
+            var video = (VideoItem)pb.Tag;
+
+            
+            VideoPlayer.URL = video.FilePath;
         }
     }
-
-    // ------- Preview animation on hover --------
-
-    private int previewFrame = 0;
-
-    private void ThumbnailMouseHover(object sender, EventArgs e)
-    {
-        currentPreviewBox = sender as PictureBox;
-        currentPreviewBox.BorderStyle = BorderStyle.Fixed3D;
-
-        previewTimer.Start();
-    }
-
-    private void ThumbnailMouseLeave(object sender, EventArgs e)
-    {
-        previewTimer.Stop();
-
-        var pb = sender as PictureBox;
-        pb.BorderStyle = BorderStyle.FixedSingle;
-
-        pb.Image = Properties.Resources.default_thumb;
-    }
-
-    private void PreviewTimerTick(object sender, EventArgs e)
-    {
-        if (currentPreviewBox == null) return;
-
-        previewFrame++;
-
-        // simple blinking effect
-        currentPreviewBox.BackColor =
-            (previewFrame % 2 == 0 ? Color.LightGray : Color.White);
-    }
-
-    // ------- Click to play video on the right panel --------
-
-    private void ThumbnailClick(object sender, EventArgs e)
-    {
-        var pb = sender as PictureBox;
-        var video = (VideoItem)pb.Tag;
-
-        videoPlayer.URL = video.FilePath;
-        videoPlayer.Ctlcontrols.play();
-    }
 }
-
--- ConsumerGUI/VideoItem.cs
+++ ConsumerGUI/VideoItem.cs
@@ -1,11 +1,14 @@
-﻿public class VideoItem
+﻿namespace ConsumerGUI
 {
-    public string FileName { get; }
-    public string FilePath { get; }
-
-    public VideoItem(string fileName, string filePath)
+    public class VideoItem
     {
-        FileName = fileName;
-        FilePath = filePath;
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+
+        public VideoItem(string fileName, string filePath)
+        {
+            FileName = fileName;
+            FilePath = filePath;
+        }
     }
 }
